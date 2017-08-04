846e2bda4c8989109167c999e244f83627e03b3d
TimerChen <TimeMachine@sjtu.edu.cn>
2017-08-04 21:05:48 +0800

    A failed LightDS test.

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,64 @@
+#include <cstdio>
+#include <iostream>
+#include <fstream>
+
+#include <tests/LightDSTest/servera.h>
+
+using namespace std;
+int main(int argc, char**argv)
+{
+	int i,j;
+	std::uint16_t port, toPort;
+	//cout << "Input your port:" ;
+	//cin >> port;
+	//cout << argc << endl;
+	if(argc==1)
+	{
+		port = 12345;
+	}else{
+		port = 12346;
+		//port = atoi(argv[1]);
+	}
+
+	ofstream logfile("test_log.txt",ios::out);
+	LightDS::Service srv("TimerServer", logfile, cin, cout, port);
+
+	ServerA sa(srv);
+	//srv.Run();
+	std::string msg;
+	if(argc==1)
+	{
+		//srv.LightDS::User::Run();
+		std::thread srvThd(&LightDS::Service::Run, &srv);
+		//srvThd.detach();
+		srvThd.join();
+		//sa.sendMessage( 12346, "Hello" );
+
+	}else{
+		//srv.LightDS::User::Run();
+		std::thread srvThd(&LightDS::Service::Run, &srv);
+		srvThd.detach();
+		sa.sendMessage( 12345, "Hello" );
+		while( 1 ){
+			std::this_thread::yield();
+			/*
+			cout << "Input...:\n\t";
+			cin >> toPort >> msg;
+			cout << "Sending....\n";
+			sa.sendMessage( toPort, msg );*/
+		}
+	}
+
+	logfile.close();
+
+	return 0;
+}
+
+/*
 #include <gtest/gtest.h>
 
 int main(int argc, char **argv) {
-	::testing::InitGoogleTest(&argc, argv);
-	return RUN_ALL_TESTS();
+	//::testing::InitGoogleTest(&argc, argv);
+	//return RUN_ALL_TESTS();
 }
+*/
--- src/tests/LightDSTest/servera.cpp
+++ src/tests/LightDSTest/servera.cpp
@@ -12,15 +12,20 @@ ServerA::ServerA( LightDS::Service &Srv )
 
 AFS::GFSError ServerA::PRCHello( std::string words )
 {
+	std::cerr << "Be called!" << std::endl;
 	std::cout << srv.getRPCCaller() << ":\n\t" << words << std::endl;
-	return {AFS::GFSErrorCode::OK, "OK!"};
+	return {AFS::GFSErrorCode::OK, "OK! The same hello to you!"};
 }
 void ServerA::sendMessage( short port, std::string words )
 {
 	//AFS::GFSError er =
 	msgpack::object_handle er =
-		srv.RPCCall( {0,static_cast<uint16_t>(port)}, "Msg", words );
-	std::cerr << er.get() << std::endl;
+		srv.RPCCall( {"192.168.1.105",static_cast<uint16_t>(port)}, "Msg", words );
+
+	//std::cerr << er.get() << " " << er.get() << std::endl;
+	AFS::GFSError reData;
+	er.get().convert( reData );
+	std::cerr << reData.description << std::endl;
 	//er.get().as<Type>();
 
 }
