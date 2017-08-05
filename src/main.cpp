38d68060e68678a2749c4fde90f431967e0d3ddf
TimerChen <TimeMachine@sjtu.edu.cn>
2017-08-06 03:34:12 +0800

    ready for rest part

--- src/main.cpp
+++ src/main.cpp
@@ -4,20 +4,20 @@
 
 #include <tests/LightDSTest/servera.h>
 
+#include <master.h>
+#include <client.h>
+#include <chunkserver.h>
+
 using namespace std;
 int main(int argc, char**argv)
 {
 
 	int i,j;
 	std::uint16_t port, toPort;
-	//cout << "Input your port:" ;
-	//cin >> port;
-	//cout << argc << endl;
 	if(argc==1)
 	{
 		port = 12308;
 	}else{
-		//port = 12346;
 		port = atoi(argv[1]);
 	}
 
@@ -26,43 +26,15 @@ int main(int argc, char**argv)
 	LightDS::Service srv("TimerServer", logfile, ss, ss, port);
 
 	ServerA sa(srv);
-	//srv.Run();
-	std::string msg;
-
-	//srv.LightDS::User::Run();
 	std::thread srvThd(&LightDS::Service::Run, &srv);
 
-	srv.Run();
 
-	std::string ip;
-	cout << "Set your friend's IP and Port.\n";
-	cin >> ip >> toPort;
-	cout << "OK..\n";
-	//getline(cin,msg);
-	//cout << msg << " " << msg.size();
-
-	while( 1 )
-	{
-		//cout << ">";
-		while(1)
-		{
-			getline(cin,msg);
-			if(msg.size() > 0)
-				break;
-		}
-		sa.sendMessage( ip, toPort, msg );
-
-		//if(...)
-		//	break;
-
-	}
 
 
 	srv.Stop();
 	ss << "{\"type\":\"bye\"}\n";
 
 	srvThd.join();
-	logfile.close();
 
 	return 0;
 }
